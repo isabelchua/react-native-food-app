95283827f1f3c1f5da882839bc1b9bcd2dd82d85
Isabel <isabelchua86@gmail.com>
2022-05-23 12:51:35 -0700

    cleaning app

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import Header from "./src/components/Header";
 import Search from "./src/components/Search";
 import CategoryItem from "./src/components/CategoryItem";
+import Categories from "./src/components/Categories";
 
 export default function App() {
 	// let term = "Bruger"
@@ -38,46 +39,15 @@ export default function App() {
 		<View style={styles.container}>
 			<Header />
 			<Search setTerm={setTerm} />
-			<FlatList
-				data={commonCategories}
-				renderItem={({ item, index }) => {
-					return (
-						<CategoryItem
-							name={item.name}
-							imageUrl={item.imageUrl}
-							index={index}
-							active={item.name === term}
-							handlePress={() => setTerm(item.name)}
-						/>
-					);
-				}}
-				horizontal
-				showsHorizontalScrollIndicator={false}
-				keyExtractor={category => category.name}
+			<Categories
+				categories={commonCategories}
+				setTerm={setTerm}
+				term={term}
 			/>
-			{/* <CategoryItem
-				name="Burger"
-				imageUrl={require("./src/assets/images/burger.png")}
-			/>
-			<CategoryItem
-				name="Pizza"
-				imageUrl={require("./src/assets/images/pizza.png")}
-			/>
-			<CategoryItem
-				name="Dessert"
-				imageUrl={require("./src/assets/images/cake.png")}
-			/> */}
-			{/* <Text>Hello! test</Text> */}
+
 			<StatusBar />
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	// container: {
-	// 	flex: 1,
-	// 	backgroundColor: "#fff",
-	// 	alignItems: "center",
-	// 	justifyContent: "center"
-	// }
-});
+const styles = StyleSheet.create({});
---
+++ src/components/Categories.js
@@ -0,0 +1,23 @@
+import CategoryItem from "./CategoryItem";
+import { FlatList } from "react-native";
+export default function Categories({ categories, setTerm, term }) {
+	return (
+		<FlatList
+			data={categories}
+			renderItem={({ item, index }) => {
+				return (
+					<CategoryItem
+						name={item.name}
+						imageUrl={item.imageUrl}
+						index={index}
+						active={item.name === term}
+						handlePress={() => setTerm(item.name)}
+					/>
+				);
+			}}
+			horizontal
+			showsHorizontalScrollIndicator={false}
+			keyExtractor={category => category.name}
+		/>
+	);
+}

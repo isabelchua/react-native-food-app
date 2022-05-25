9ca5e9f17340cf08437e1ff2c5b34500067425b9
isabelchua <isabelchua86@gmail.com>
2022-05-25 13:00:31 -0700

    restaurant screen

--- App.js
+++ App.js
@@ -7,48 +7,39 @@ import CategoryItem from "./src/components/CategoryItem";
 import Categories from "./src/components/Categories";
 import Restaurants from "./src/components/Restaurants";
 
-export default function App() {
-	// let term = "Bruger"
-	const [term, setTerm] = useState("Burger");
-	const commonCategories = [
-		{
-			name: "Burger",
-			imageUrl: require("./src/assets/images/burger.png")
-		},
-		{
-			name: "Pizza",
-			imageUrl: require("./src/assets/images/pizza.png")
-		},
-		{
-			name: "Dessert",
-			imageUrl: require("./src/assets/images/cake.png")
-		},
-		{
-			name: "Pasta",
-			imageUrl: require("./src/assets/images/pasta.png")
-		},
-		{
-			name: "Steak",
-			imageUrl: require("./src/assets/images/steak.png")
-		},
-		{
-			name: "Drinks",
-			imageUrl: require("./src/assets/images/smoothies.png")
-		}
-	];
-	return (
-		<View style={styles.container}>
-			<Header />
-			<Search setTerm={setTerm} />
-			<Categories
-				categories={commonCategories}
-				setTerm={setTerm}
-				term={term}
-			/>
-			<Restaurants term={term} />
-			<StatusBar />
-		</View>
-	);
-}
+// export default function App() {
+// 	// let term = "Bruger"
 
-const styles = StyleSheet.create({});
+// 	return (
+// 		<View>
+// 			<Header />
+// 			<Search setTerm={setTerm} />
+// 			<Categories
+// 				categories={commonCategories}
+// 				setTerm={setTerm}
+// 				term={term}
+// 			/>
+// 			<Restaurants term={term} />
+// 			<StatusBar />
+// 		</View>
+// 	);
+// }
+
+// const styles = StyleSheet.create({});
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator(
+	{
+		Home: HomeScreen
+	},
+	{
+		initialRouteName: "Home",
+		defaultNavigationOptions: {
+			title: "BusinessSearch"
+		}
+	}
+);
+
+export default createAppContainer(navigator);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,42 @@
+import { View, StyleSheet } from "react-native";
+import Header from "./../components/Header";
+import Search from "./../components/Search";
+import Categories from "./../components/Categories";
+import Restaurants from "./../components/Restaurants";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+
 export default function HomeScreen() {
-	import { View } from "react-native";
-	import Header from "./../components/Header";
-	import Search from "./../components/Search";
-	import Categories from "./../components/Categories";
-	import Restaurants from "./../components/Restaurants";
-	import { StatusBar } from "expo-status-bar";
+	const [term, setTerm] = useState("Burger");
+	const commonCategories = [
+		{
+			name: "Burger",
+			imageUrl: require("../assets/images/burger.png")
+		},
+		{
+			name: "Pizza",
+			imageUrl: require("../assets/images/pizza.png")
+		},
+		{
+			name: "Dessert",
+			imageUrl: require("../assets/images/cake.png")
+		},
+		{
+			name: "Pasta",
+			imageUrl: require("../assets/images/pasta.png")
+		},
+		{
+			name: "Steak",
+			imageUrl: require("../assets/images/steak.png")
+		},
+		{
+			name: "Drinks",
+			imageUrl: require("../assets/images/smoothies.png")
+		}
+	];
+
 	return (
-		<View>
+		<View style={styles.container}>
 			<Header />
 			<Search setTerm={setTerm} />
 			<Categories
@@ -19,3 +49,10 @@ export default function HomeScreen() {
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "rgb(253,253,253)",
+		flex: 1
+	}
+});
